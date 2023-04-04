0ccca099ed6e5c7ba8932d1b9e30ae53421bc059
LeShaDDoW <92387799+LeShaDDoW@users.noreply.github.com>
2023-04-04 20:02:30 +0200

    Made new Pickup Script after it was seemingly deleted
    I just winged up a new script it isnt finished tho and but I just wanted to have it committed cuz I can

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  Author: LeShaDDoW2
+ */
+
+public class Pickup : MonoBehaviour
+{
+        // Variables
+    public GameObject holdingPoint;
+    public float distance;
+    public LayerMask layerMask;
+    public LineRenderer lineRenderer;
+    public float speed;
+
+    Rigidbody pickupRb;
+    GameObject pickupObject;
+
+    bool lineEnabled;
+    bool isHolding;
+
+    void Start()
+    {
+        lineEnabled = false;
+    }
+
+    void Update()
+    {
+        MyInput();
+        LineRendering();
+        ObjectMoving();
+    }
+
+    void MyInput()
+    {
+            // Picking Up
+        /*  
+         *  When you click down the right mouse button:
+         *    Cast a Ray from the Camera forwards and if it hits something that you can pickup,
+         *    call the Pickup Function with the rigidbody of the hit Object.
+         */
+        RaycastHit hit;
+        if(Input.GetMouseButtonDown(1) && 
+            Physics.Raycast(transform.position, transform.forward, out hit, distance, layerMask) &&
+            hit.rigidbody != null) {
+                PickUp(hit.rigidbody);
+            }
+
+            // Dropping
+        /*
+         *  When Letting go of the Right Mouse Button, call the Drop function.
+         */
+        if(Input.GetMouseButtonUp(1) && pickupObject != null) 
+        {
+            Drop();
+        }
+    }
+
+    void Drop()
+    {
+            // Resetting all variables about the picked up object
+        pickupObject = null;
+        pickupRb = null;
+
+        lineEnabled = false;
+        isHolding = false;
+
+            // Resetting the line renderer
+        lineRenderer.positionCount = 0;
+    }
+
+    void PickUp(Rigidbody hitRb)
+    {
+            // Setting the variables for the picked up Object
+        pickupObject = hitRb.gameObject;
+        pickupRb = hitRb;
+
+            // Enable Holding and the line renderer
+        lineEnabled = true;
+        isHolding = true;
+    }
+
+    void LineRendering()
+    {
+             // Setting the Line Renderers positions to connect the picked up objects
+             // and the area which it is moving to.
+        if(lineEnabled)
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, holdingPoint.transform.position);
+            lineRenderer.SetPosition(1, pickupObject.transform.position);
+        }
+    }
+
+    void ObjectMoving()
+    {
+            // Seting the velocity of the picked up Object to the distance between the holding point and the object
+        if(pickupRb != null)
+        {
+            Vector3 distance = (holdingPoint.transform.position - pickupRb.transform.position) * speed;
+            pickupRb.velocity = distance;
+        }
+    }
+}
